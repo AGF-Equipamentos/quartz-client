f2d0f601cdc78ebb9d702777dda324572b956270
Alana Beatriz <alanabeatrizcourela931@gmail.com>
2022-04-26 17:29:23 -0300

    Table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
 import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
-import { useTable, useSortBy, Column } from 'react-table'
+import { useTable, Column, usePagination, useSortBy } from 'react-table'
 
 export type DataTableProps<Data extends object> = {
   data: Data[]
@@ -13,48 +13,135 @@ export default function DataTable<Data extends object>({
   data,
   columns
 }: DataTableProps<Data>) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy)
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
+  } = useTable(
+    { columns, data, initialState: { pageIndex: 0 } },
+    useSortBy,
+    usePagination
+  )
 
   return (
-    <Table {...getTableProps()}>
-      <Thead>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <Th
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                isNumeric={column.isNumeric}
-              >
-                {column.render('Header')}
-                <chakra.span pl="4">
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <TriangleDownIcon aria-label="sorted descending" />
-                    ) : (
-                      <TriangleUpIcon aria-label="sorted ascending" />
-                    )
-                  ) : null}
-                </chakra.span>
-              </Th>
-            ))}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
-          return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
-                  {cell.render('Cell')}
-                </Td>
+    <>
+      <pre>
+        <code>
+          {/* {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage
+            },
+            null,
+            2
+          )} */}
+        </code>
+      </pre>
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
+                >
+                  {column.render('Header')}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
               ))}
             </Tr>
-          )
-        })}
-      </Tbody>
-    </Table>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row)
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <Td
+                    {...cell.getCellProps()}
+                    isNumeric={cell.column.isNumeric}
+                  >
+                    {cell.render('Cell')}
+                  </Td>
+                ))}
+              </Tr>
+            )
+          })}
+        </Tbody>
+      </Table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{
+              width: '100px',
+              color: 'white',
+              background: '#2D3748'
+            }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          style={{ color: 'white', background: '#2D3748' }}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   )
 }
--- src/components/Table/mock.tsx
+++ src/components/Table/mock.tsx
@@ -31,7 +31,7 @@ const data = [
     tags: 'pedido CE26p',
     month: 'Junho',
     observation: 'Não deu certo',
-    delivery: '19/04/2022',
+    delivery: '2022-03-28T16:37:45.333Z',
     status: 'Cotação feita',
     bought: 'Kevin',
     approved: 'Não'
@@ -42,7 +42,7 @@ const data = [
     tags: 'pedido CE27p',
     month: 'Julho',
     observation: 'Deu certo',
-    delivery: '18/04/2022',
+    delivery: '2022-03-28T16:37:45.333Z',
     status: 'Aguardando cotação',
     bought: 'Alana',
     approved: 'Sim'
@@ -53,7 +53,7 @@ const data = [
     tags: 'pedido CE28p',
     month: 'Agosto',
     observation: 'Tá quase dando certo',
-    delivery: '17/04/2022',
+    delivery: '2022-03-28T16:37:45.333Z',
     status: 'Aguardando cotação',
     bought: 'Bruna',
     approved: 'Não'
@@ -64,6 +64,226 @@ const data = [
     tags: 'pedido CE29p',
     month: 'Setembro',
     observation: 'Não deu certo',
+    delivery: '2022-03-28T16:37:45.333Z',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '006',
+    provider: 'Isabela',
+    tags: 'pedido CE29p',
+    month: 'Setembro',
+    observation: 'Não deu certo',
+    delivery: '2022-03-28T16:37:45.333Z',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '007',
+    provider: 'Gustavo',
+    tags: 'pedido CE29p',
+    month: 'Setembro',
+    observation: 'Não deu certo',
+    delivery: '2022-03-28T16:37:45.333Z',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '008',
+    provider: 'Ana Laura',
+    tags: 'pedido CE29p',
+    month: 'Setembro',
+    observation: 'Não deu certo',
+    delivery: '2022-03-28T16:37:45.333Z',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '009',
+    provider: 'Maria Laura',
+    tags: 'pedido CE29p',
+    month: 'Setembro',
+    observation: 'Não deu certo',
+    delivery: '2022-03-28T16:37:45.333Z',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '010',
+    provider: 'Gabriel',
+    tags: 'pedido CE29p',
+    month: 'Setembro',
+    observation: 'Não deu certo',
+    delivery: '2022-03-28T16:37:45.333Z',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '011',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '012',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '013',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '014',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '015',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '016',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '017',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '018',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '019',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '020',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '021',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '022',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '023',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '024',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
+    delivery: '16/04/2022',
+    status: 'Cotação feita',
+    bought: 'Ronaldo',
+    approved: 'Sim'
+  },
+  {
+    number: '025',
+    provider: 'Bruno',
+    tags: 'pedido CE39p',
+    month: 'Janeiro',
+    observation: 'Deu certo',
     delivery: '16/04/2022',
     status: 'Cotação feita',
     bought: 'Ronaldo',
@@ -84,6 +304,7 @@ const columns: Column<UnitConversion>[] = [
     Header: 'Tags',
     accessor: 'tags'
   },
+
   {
     Header: 'Mês',
     accessor: 'month'
@@ -112,5 +333,4 @@ const columns: Column<UnitConversion>[] = [
     Header: 'Ações'
   }
 ]
-
 export { data, columns }

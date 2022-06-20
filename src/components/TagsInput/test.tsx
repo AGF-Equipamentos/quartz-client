30c436a901b17e988e84ebd0e5d1d9c1b6d43e9c
Alana Beatriz <alanabeatrizcourela931@gmail.com>
2022-06-20 17:28:57 -0300

    The "should not render empty" test was taken.

--- src/components/TagsInput/test.tsx
+++ src/components/TagsInput/test.tsx
@@ -1,10 +1,11 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import { TagsInput } from '.'
-const Tags = ['CE25P', 'MF75P2', 'CE28P']
+
+const tags = ['CE25P', 'MF75P2', 'CE28P']
 
 describe('<TagsInput />', () => {
   it('should render the "defaultValues" of the tags', () => {
-    render(<TagsInput setValue={() => true} initialTags={Tags} />)
+    render(<TagsInput setValue={() => true} initialTags={tags} />)
 
     expect(screen.getByText('MF75P2')).toBeInTheDocument()
     expect(screen.getByText('CE25P')).toBeInTheDocument()
@@ -12,7 +13,7 @@ describe('<TagsInput />', () => {
   })
 
   it('should delete a tag', () => {
-    render(<TagsInput setValue={() => true} initialTags={Tags} />)
+    render(<TagsInput setValue={() => true} initialTags={tags} />)
     const button = screen.getByText('MF75P2').nextSibling
 
     if (button === null) return
@@ -24,44 +25,49 @@ describe('<TagsInput />', () => {
   it('should create new tag', () => {
     render(<TagsInput setValue={() => true} />)
 
-    const TagInput = screen.getByPlaceholderText('Adicionar Tag...')
-    fireEvent.change(TagInput, { target: { value: 'alana' } })
+    const tagInput = screen.getByPlaceholderText('Adicionar Tag...')
+    fireEvent.change(tagInput, { target: { value: 'alana' } })
 
-    fireEvent.keyDown(TagInput, { key: 'Enter' })
+    fireEvent.keyDown(tagInput, { key: 'Enter' })
     expect(screen.getByText('alana')).toBeInTheDocument()
   })
 
   it('should not be possible to tighten another tacla be to be the "Enter"', () => {
     render(<TagsInput setValue={() => true} />)
 
-    const TagInput = screen.getByPlaceholderText('Adicionar Tag...')
-    fireEvent.change(TagInput, { target: { value: 'alana' } })
+    const tagInput = screen.getByPlaceholderText('Adicionar Tag...')
+    fireEvent.change(tagInput, { target: { value: 'alana' } })
 
-    fireEvent.keyDown(TagInput, { key: 'Espaço' })
+    fireEvent.keyDown(tagInput, { key: 'Espaço' })
     expect(screen.queryByText('alana')).not.toBeInTheDocument()
   })
 
-  //usar o getAll, e usar a proriedade roHaveLength
   it('should not render empty', () => {
-    render(<TagsInput setValue={() => true} />)
+    render(<TagsInput setValue={() => true} initialTags={tags} />)
 
-    const TagInput = screen.getByPlaceholderText('Adicionar Tag...')
-    fireEvent.change(TagInput, { target: { value: ' ' } })
+    const tagInput = screen.getByPlaceholderText('Adicionar Tag...')
+    fireEvent.change(tagInput, { target: { value: '  ' } })
 
-    fireEvent.keyDown(TagInput, { key: 'Enter' })
-    // expect(screen.queryByText(' ')).not.toBeInTheDocument()
-    // const Tags = ['CE25P', 'MF75P2', 'CE28P']
+    fireEvent.keyDown(tagInput, { key: 'Enter' })
 
-    // expect(screen.getAllByText('Tags')).toHaveLength(1)
+    expect(screen.getAllByRole('button')).toHaveLength(3)
   })
 
-  // it('should create new tag', () => {
-  //   const setValue = jest.fn()
-  //   // initial tags
-  //   // adicionar
-  //   // remove tag
-  //   render(<TagsInput setValue={setValue} initialTags={Tags} />)
+  // const tags = ['CE25P', 'MF75P2', 'CE28P']
 
-  //   expect(setValue).toHaveBeenCalledWith('tags', 'alana;arthur')
-  // })
+  it('should trigger the "setValue" function', () => {
+    const setValue = jest.fn()
+    // initial tags
+    render(<TagsInput setValue={setValue} initialTags={tags} />)
+    expect(setValue).toHaveBeenCalledWith('tags', tags.join(';'))
+
+    // add tag
+    const tagInput = screen.getByPlaceholderText('Adicionar Tag...')
+    fireEvent.change(tagInput, { target: { value: 'bia' } })
+
+    fireEvent.keyDown(tagInput, { key: 'Enter' })
+    // expect(setValue).toHaveBeenCalledWith()
+
+    // remove tag
+  })
 })

3361e3e4a0de8921e4c57ec87556fe2fb4ed0738
Alana Beatriz <alanabeatrizcourela931@gmail.com>
2022-06-27 11:47:37 -0300

    Filter

--- src/components/Table/Filter/test.tsx
+++ src/components/Table/Filter/test.tsx
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import FilterModal from '.'
 
 describe('< FilterModal />', () => {
-  it('should to filter by number', () => {
-    render(<FilterModal isOpen={true} handleClose={() => false} />)
+  it('should trigger the "handleFilter"', () => {
+    const handleFilter = jest.fn()
+
+    render(
+      <FilterModal
+        isOpen={true}
+        handleClose={() => false}
+        handleFilter={handleFilter}
+      />
+    )
+    const observationInput = screen.getByLabelText('Número')
+    fireEvent.change(observationInput, { target: { value: '011' } })
+
+    const button = screen.getByText('Filtrar')
+    fireEvent.click(button)
+
+    // expect(handleFilter).
   })
 })
